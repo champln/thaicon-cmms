7349e802cf975892b7e80ae459e8701077ba03bc
champln <jamadhar99@hotmail.com>
2026-08-12 22:07:17 +0700

    Initialize ThaiCon CMMS

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@fontsource-variable/anuphan";
+import CMMSApp from "./CMMSApp";
+import "./global.css";
+import "./cmms.css";
+import "./iot-monitor.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <CMMSApp />
+  </StrictMode>,
+);
